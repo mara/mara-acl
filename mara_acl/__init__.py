bcb28b232c5ba2f68320f6e5ddea7336e0af9e3d
Jan Katins <jasc@gmx.net>
2018-05-13 18:29:08 +0200

    Use mara_config

--- mara_acl/__init__.py
+++ mara_acl/__init__.py
@@ -1,14 +1,24 @@
-from mara_acl import views, config, permissions, users
+
+def MARA_CONFIG_MODULES():
+    from mara_acl import config
+    return [config]
 
 
-MARA_CONFIG_MODULES = [config]
+def MARA_FLASK_BLUEPRINTS():
+    from mara_acl import views
+    return [views.blueprint]
 
-MARA_FLASK_BLUEPRINTS = [views.blueprint]
 
-MARA_AUTOMIGRATE_SQLALCHEMY_MODELS = [permissions.Permission, users.User]
+def MARA_AUTOMIGRATE_SQLALCHEMY_MODELS():
+    from mara_acl import permissions, users
+    return [permissions.Permission, users.User]
 
-MARA_ACL_RESOURCES = [views.acl_resource]
 
-MARA_CLICK_COMMANDS = []
+def MARA_ACL_RESOURCES():
+    from mara_acl import views
+    return [views.acl_resource]
 
-MARA_NAVIGATION_ENTRY_FNS = [views.navigation_entry]
+
+def MARA_NAVIGATION_ENTRY_FNS():
+    from mara_acl import views
+    return [views.navigation_entry]
--- mara_acl/config.py
+++ mara_acl/config.py
@@ -1,16 +1,21 @@
 """Acl configuration"""
 from mara_page import acl
+from mara_config import declare_config
 
+
+@declare_config()
 def resources() -> [acl.AclResource]:
     """All resources that are protected by the acl"""
     return []
 
 
+@declare_config()
 def automatically_create_accounts_for_new_users() -> bool:
     """When true, then a new account is automatically created when a new user tries to login"""
     return True
 
 
+@declare_config()
 def role_for_new_user(is_first_user: bool = False, email: str = 'foo@bar.com') -> str:
     """The role for automatically created users"""
     if is_first_user:
@@ -19,18 +24,20 @@ def role_for_new_user(is_first_user: bool = False, email: str = 'foo@bar.com') -
         return 'Guest'
 
 
+@declare_config()
 def initial_permissions() -> [[str, str]]:
     """The initial permissions that are added when the first user is created"""
     return {'admin_all': ['user__Admin', 'resource__All'],
             'guest_all': ['user__Guest', 'resource__All']}
 
 
+@declare_config()
 def email_http_header() -> str:
     """The http header that contains the email of the authenticated user"""
     return 'X_FORWARDED_EMAIL'
 
 
+@declare_config()
 def whitelisted_uris() -> [str]:
     """Which uris to exclude from the acl"""
     return ['/static/']
-
